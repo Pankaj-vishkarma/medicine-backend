5af08ad1b3834321fe7a614312d46bca704f42e9
pankaj vishwakarma <145672950+Pankaj-vishkarma@users.noreply.github.com>
2024-06-05 18:08:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors=require('cors')
 database()
 
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['https://search-dava.netlify.app'],
     methods:['GET','POST'],
     credentials:true
 }))
